cceca915adf270ed56bdb851d969a92c5f081e84
Leopold Chen <placeintime.qh@gmail.com>
2018-01-31 17:34:08 +0800

    fix route for sample-app; update readme

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 - nplc install tatfook/dove --version=0.1.0 --local
 - nplc dove init
 - nplc dove start
+- localhost:8088
+
+### Online Debugger
+- launch server
+- 127.0.0.1:8088
--- cli/sample-app/config/initializers/route.lua
+++ cli/sample-app/config/initializers/route.lua
@@ -1,14 +1,15 @@
 --[[
 desc: config your routes here.
-
 ]]
 local Route = commonlib.gettable("ActionDispatcher.Routing.Route")
 local RouteHelper = commonlib.gettable("ActionDispatcher.Routing.RouteHelper")
-local join = RouteHelper.join
 local resources = RouteHelper.resources
 local namespace = RouteHelper.namespace
+local scope = RouteHelper.scope
+local url = RouteHelper.url
+local rule = RouteHelper.rule
 
 RouteHelper.route(
     -- add your route definitions here
-    resources("home", {only = {"index"}})
+    url("get", "/", "Controller.Home", "index")
 )
