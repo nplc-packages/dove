00ebcc2f1fca30db934a09e5bf6c1050c0f00daa
Leopold Chen <placeintime.qh@gmail.com>
2018-02-02 17:34:17 +0800

    fix logger issue, dir must exist

--- npl_mod/dove/application.lua
+++ npl_mod/dove/application.lua
@@ -7,6 +7,8 @@ desc: basic handler for web application, will handle all client requestes
 local App = commonlib.inherit(nil, "Dove.Application")
 local Loader = commonlib.gettable("Dove.Utils.Loader")
 local Dispatcher = commonlib.gettable("Dove.Middleware.Dispatcher")
+local PathHelper = commonlib.gettable("Dove.Utils.PathHelper")
+local lfs = commonlib.Files.GetLuaFileSystem()
 
 App.config = {
     env = "development",
@@ -29,13 +31,22 @@ local function load_app()
     Loader.load_files("config/initializers")
 end
 
+local function change_logger_path()
+    local log_dir = PathHelper.concat(lfs.currentdir(), "log")
+    local attr = lfs.attributes(log_dir)
+    if not attr or attr.mode == "file" then
+        lfs.mkdir(log_dir)
+    end
+    commonlib.servicelog.GetLogger(""):SetLogFile(format("log/%s.log", self.config.env)) -- update log file
+end
+
 function App:info()
 end
 
 function App:start()
     NPL.load(format("config/enviroments/%s", self.config.env))
     load_app()
-    commonlib.servicelog.GetLogger(""):SetLogFile(format("log/%s.log", self.config.env)) -- update log file
+    change_logger_path()
     -- 启动web服务器
     WebServer:Start("app", "0.0.0.0", self.config.port)
     log("Application is ready!")
